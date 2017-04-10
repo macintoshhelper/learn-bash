d021e47410e18bb0c1a46bf26c1ec16496f7e265
Macintosh Helper <noreply@macintoshhelper.com>
2017-04-10 18:23:11 +0300

    TDD for 03.sh script

--- tests/03.test.sh
+++ tests/03.test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+FILEBASE="$(dirname $0)/fixtures"
+# Change FILEBASE to normal folder for main script?
 
-FILEBASE=${FILEBASE:-/usr/local/foo}
-
-echo ${FILEBASE}
-ls -l
+function list_files_with_a {
+  ls ${FILEBASE}/a*
+}
+list_files_with_a
