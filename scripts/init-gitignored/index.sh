427906dab93ffdc0d153edab549604197cfcbfd4
Macintosh Helper <noreply@macintoshhelper.com>
2017-04-10 18:29:55 +0300

    Remove broken line breaks from init-gitignored

--- scripts/init-gitignored/index.sh
+++ scripts/init-gitignored/index.sh
@@ -4,12 +4,12 @@ read client_id
 echo 'Enter client_secret and press [ENTER]'
 read client_secret
 
-echo 'Enter secret (can use anything) and press [ENTER]'
+echo 'Enter secret and press [ENTER]'
 read secret
 
 
-echo "BASE_URL=https://localhost:3000\n
-CLIENT_ID=$client_id\n
-CLIENT_SECRET=$client_secret\n
-SECRET=$secret\n
+echo "BASE_URL=https://localhost:3000
+CLIENT_ID=$client_id
+CLIENT_SECRET=$client_secret
+SECRET=$secret
 " > config.env
